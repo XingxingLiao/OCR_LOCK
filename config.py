de2368f75144f3d628a1d56432bf6dc4fe040021
XingxingLiao <liaoxingxin498@gmail.com>
2025-07-01 10:28:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,15 @@ url = "https://api.ocr.space/parse/image"
 api_key = "K87912659088957"
 image_path = "/home/CITSEM/ocr/OCR_LOCK/image/image_000.jpg"
 
-mqtt_broker = "138.100.58.174"
-mqtt_port = 1883
+# mqtt_broker = "138.100.58.174"
+# mqtt_port = 1883
+# mqtt_topic = "ocr/data"
+
+mqtt_broker = "0eb00aca75ab4f56b36f74924b54b76b.s1.eu.hivemq.cloud" 
+mqtt_port = 8883
 mqtt_topic = "ocr/data"
+mqtt_username = "CITSEM"
+mqtt_password = "Admin123"
 
 crop_coordinates = [
     (409, 624, 245, 112),   # Region 1
