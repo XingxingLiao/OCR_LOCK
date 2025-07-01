3fe7b03f88110822ee7f6092baf9a155bac5c9f6
XingxingLiao <liaoxingxin498@gmail.com>
2025-07-01 10:29:42 +0200

    Update mqtt_publisher.py

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -1,9 +1,34 @@
-import paho.mqtt.client as mqtt
-import json
-from config import mqtt_broker, mqtt_port, mqtt_topic
+# import paho.mqtt.client as mqtt
+# import json
+# from config import mqtt_broker, mqtt_port, mqtt_topic
 
-def send_mqtt_message(message):
+# def send_mqtt_message(message):
+#     client = mqtt.Client()
+#     client.connect(mqtt_broker, mqtt_port, 60)
+#     client.publish(mqtt_topic, json.dumps(message))
+#     client.disconnect()
+# mqtt_sender.py
+
+import paho.mqtt.client as mqtt
+import ssl
+import json
+from config import mqtt_broker, mqtt_port, mqtt_topic, mqtt_username, mqtt_password
+
+def publish_message(payload):
     client = mqtt.Client()
-    client.connect(mqtt_broker, mqtt_port, 60)
-    client.publish(mqtt_topic, json.dumps(message))
-    client.disconnect()
+
+    client.tls_set(tls_version=ssl.PROTOCOL_TLS)
+
+
+    client.username_pw_set(mqtt_username, mqtt_password)
+
+    # 连接并发布
+    try:
+        client.connect(mqtt_broker, mqtt_port)
+        client.loop_start()
+        client.publish(mqtt_topic, payload)
+        client.loop_stop()
+        client.disconnect()
+        print(f"succeed in sending {mqtt_topic}: {payload}")
+    except Exception as e:
+        print(f"fail to send data: {e}")
